141be7c0db342612d8a9ec5ae43bfbb8907930d5
prplecake <me@prplecake.com>
2023-06-16 11:24:52 -0500

    Add testing client

---
+++ TestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Bunny.NET.Client;
+using Serilog;
+using TestClient;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+Console.WriteLine("Hello, World!");
+
+var config = Config.SetupConfiguration();
+
+var bun = new BunnyClient(config.ApiToken);
+var zones = bun.GetZones().Result;
+
+Console.WriteLine();
