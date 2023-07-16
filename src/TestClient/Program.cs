cc01d403503c1cc12713dd0bfe81c43043357c53
Matty Jorgensen <me@prplecake.com>
2023-07-16 18:32:48 -0500

    Init logging API

--- src/TestClient/Program.cs
+++ src/TestClient/Program.cs
@@ -15,7 +15,7 @@ var config = Config.SetupConfiguration();
 
 var bun = new BunnyClient(config.ApiToken);
 // var zones = bun.GetZones().Result;
-
-var statistics = bun.GetStatistics().Result;
+// var statistics = bun.GetStatistics().Result;
+var logFile = bun.GetLogFile(DateOnly.FromDateTime(DateTime.Today), "1450520").Result;
 
 Console.WriteLine();
