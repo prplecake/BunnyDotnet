eb6f8d60c2e7c8c7a12a85348273126ab04e5d7a
Matty Jorgensen <me@prplecake.com>
2023-11-11 14:00:28 -0600

    Add BunnyClient.Countries tests

--- src/Bunny.NET/Client/BunnyClient.Countries.cs
+++ src/Bunny.NET/Client/BunnyClient.Countries.cs
@@ -6,12 +6,13 @@ namespace Bunny.NET.Client;
 partial class BunnyClient
 {
     private string _countriesApiUrl;
-    public async Task<List<Country>> GetCountryList()
+    public async Task<Result<List<Country>>> GetCountryList()
     {
         var response = await Client.GetAsync(_countriesApiUrl);
-        response.EnsureSuccessStatusCode();
+        if (!response.IsSuccessStatusCode)
+            return new Result<List<Country>> { StatusCode = response.StatusCode, Success = false };
         string responseContent = await response.Content.ReadAsStringAsync();
         var obj = JsonConvert.DeserializeObject<List<Country>>(responseContent);
-        return obj;
+        return new Result<List<Country>> { StatusCode = response.StatusCode, Success = true, Data = obj };
     }
 }
